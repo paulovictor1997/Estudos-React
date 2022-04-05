2e035313b68f2ea51c1920efd32c4e4fbb86013c
paulovictor1997 <paulovictornunes97@gmail.com>
2022-04-05 00:05:36 -0300

    Atividade-1

--- atividade-1/src/App.jsx
+++ atividade-1/src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import './App.css'
 import Cards from "./Componentes/Cards";
-//import sedans from './images/icon-sedans.svg';
-//import suvs from './images/icon-suvs.svg';
-//import luxury from './images/icon-luxury.svg';
+
 
 export default (props) =>{
     return(
