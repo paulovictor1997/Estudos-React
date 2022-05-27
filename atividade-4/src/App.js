78d18d5697257edb383074449074bf17a274af3b
paulovictor1997 <paulovictornunes97@gmail.com>
2022-05-27 19:23:51 -0300

    Update atividade-4

--- atividade-4/src/App.js
+++ atividade-4/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Paulo Victor';
+
+  function soma(a,b){
+    return a + b
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>TESTANDO NOVAMENTE ALGUNS COMANDOS</h4>
+      <p>Olá {name}, seja bem Vindo</p>
+      <p>Soma é : {soma(1,2)}</p>
     </div>
   );
 }
