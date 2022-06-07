3c48ee8826e0cad1f748f293998cc3c0f3f38c1a
paulovictor1997 <paulovictornunes97@gmail.com>
2022-06-06 21:18:30 -0300

    Update - Curso

---
+++ Curso-React/meu-projeto/src/componentes/layout/LinkButton.jsx
@@ -0,0 +1,10 @@
+import './LinkButton.css'
+import { Link } from 'react-router-dom'
+
+export default function LinkButton({to,text}){
+    return(
+        <Link className='button' to={to}>
+          {text}
+        </Link>
+    )
+}
--- Curso-React/meu-projeto/src/componentes/pages/Home.jsx
+++ Curso-React/meu-projeto/src/componentes/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import './Home.css';
+import moeda from '../../img/moeda.jpg';
+import LinkButton from "../layout/LinkButton";
 
 export default function Home(){
     return(
-        <><h1>Home</h1></>
+        <section className="home_content">
+            <h1>Seja bem-vindo ao <span>Cots</span></h1>
+            <p>Comece a gerenciar seus projetos agora mesmo !</p>
+           <LinkButton to='/Projetos' text='Criar Projeto'/>
+            <img src={moeda} alt="moeda" />
+        </section>
         
     )
 }
