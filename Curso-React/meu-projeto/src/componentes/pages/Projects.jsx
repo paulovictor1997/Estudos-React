69c243ec69bae3644379c5192f9bc34eaa9e2b63
paulovictor1997 <paulovictornunes97@gmail.com>
2022-06-14 19:45:57 -0300

    Update - Curso - Dashbord

--- Curso-React/meu-projeto/src/componentes/pages/Projects.jsx
+++ Curso-React/meu-projeto/src/componentes/pages/Projects.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
+import './Projects.css';
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
 
 export default function Projects(){
     const location = useLocation()
@@ -10,9 +13,15 @@ export default function Projects(){
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className="project_container">
+            <div className="title_container">
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/projetos' text='Criar Projeto'/>
+            </div>
             {message && <Message type='success' msg = {message}/>}
+            <Container className = 'start'>
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
