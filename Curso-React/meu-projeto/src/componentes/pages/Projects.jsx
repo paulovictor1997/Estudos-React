f2a7a1ff9519636e0a729ad3148449757e0c046b
paulovictor1997 <paulovictornunes97@gmail.com>
2022-06-14 21:35:08 -0300

    Update - Curso

--- Curso-React/meu-projeto/src/componentes/pages/Projects.jsx
+++ Curso-React/meu-projeto/src/componentes/pages/Projects.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
 import './Projects.css';
 import Container from '../layout/Container';
 import LinkButton from '../layout/LinkButton';
+import ProjectCard from "../projeto_form/ProjectCard";
 
 export default function Projects(){
+    //Criando os projetos
+    const [projects,setProjects] = useState([])
+
     const location = useLocation()
     let message = ''
     if(location.state){
         message = location.state.message
     }
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/projects',{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+            },
+        })
+        .then((resp)=>resp.json())
+        .then((data)=>{
+            console.log(data)
+            setProjects(data);
+        })
+        .catch((error)=>console.log(error))
+    },[])
 
     return(
         <div className="project_container">
@@ -20,7 +39,14 @@ export default function Projects(){
             </div>
             {message && <Message type='success' msg = {message}/>}
             <Container className = 'start'>
-                <p>Projetos...</p>
+                {projects.length > 0 && 
+                  projects.map((project)=> <ProjectCard name={project.name}
+                  budget={project.budget}
+                  category={project.category.name}
+                  key={project.id}
+                  
+                  />)  
+                }
             </Container>
         </div>
     )
