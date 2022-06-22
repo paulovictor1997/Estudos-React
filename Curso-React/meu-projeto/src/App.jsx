622bc095441e389890528a7b98178f18df4248d9
paulovictor1997 <paulovictornunes97@gmail.com>
2022-06-22 18:21:14 -0300

    Update - Curso

--- Curso-React/meu-projeto/src/App.jsx
+++ Curso-React/meu-projeto/src/App.jsx
@@ -1,38 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import Home from './componentes/pages/Home';
-import Empresa from './componentes/pages/Empresa';
-import Contato from './componentes/pages/Contato';
-import Projetos from './componentes/pages/Projetos';
-import Projects from './componentes/pages/Projects';
-import Container from './componentes/layout/Container';
-import Navbar from './componentes/layout/Navbar';
-import Footer from './componentes/layout/Footer';
-import Project from './componentes/pages/Project';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./componentes/pages/Home";
+import Empresa from "./componentes/pages/Empresa";
+import Contato from "./componentes/pages/Contato";
+import Projetos from "./componentes/pages/Projetos";
+import Projects from "./componentes/pages/Projects";
+import Container from "./componentes/layout/Container";
+import Navbar from "./componentes/layout/Navbar";
+import Footer from "./componentes/layout/Footer";
+import Project from "./componentes/pages/Project";
 
 export default function App() {
-    return(
-       <>
-       <Router>
+  return (
+    <>
+      <Router>
+        <Navbar />
 
-         <Navbar/>
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/Empresa" element={<Empresa />} />
+            <Route exact path="/Contato" element={<Contato />} />
+            <Route exact path="/Projetos" element={<Projetos />} />
+            <Route exact path="/Projects" element={<Projects />} />
+            <Route exact path="/project/:id" element={<Project />} />
+          </Routes>
+        </Container>
 
-         <Container>
-           <Routes>
-               <Route exact path='/'element ={<Home/>} />
-               <Route exact path = "/Empresa" element = {<Empresa/>}/>
-               <Route exact path = "/Contato" element = {<Contato/>}/>
-               <Route exact path = "/Projetos" element = {<Projetos/>}/>
-               <Route exact path = "/Projects" element = {<Projects/>}/>
-               <Route exact path="/project/:id" element = {<Project/>}/>
-            </Routes>
-         </Container>
-
-         <Footer/>
-         
-       </Router>
-       
-       </>
-        
-    )
+        <Footer />
+      </Router>
+    </>
+  );
 }
