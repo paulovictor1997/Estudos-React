473d20558de97eed78757a7029c0f0689d4b0c2a
paulovictor1997 <paulovictornunes97@gmail.com>
2022-08-25 22:45:43 -0300

    Update - atividade-6

--- atividade-6/src/App.jsx
+++ atividade-6/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Componentes/Header';
-import Search from './Componentes/Search';
+import Navbar from './Componentes/Navbar';
 import {Outlet} from 'react-router-dom'
 
 export default function App() {
   return (
     <div className="container">
       <Header/>
-      <Search/>
+      <Navbar/>
       <Outlet/>
     </div>
   )
---
+++ atividade-6/src/Componentes/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Search(){
+    return(
+        <nav>
+            <form className='search-country' autoComplete='off'>
+                <input type="search" 
+                name='search' 
+                id='search' 
+                placeholder='Search for a country'  />
+            </form>
+
+            <div className="select-country">
+                <select name="select" id="select">
+                    <option value="Filter by region">Filter by region</option>
+                    <option value="America">America</option>
+                    <option value="Africa">Africa</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europa">Europe</option>
+                    <option value="Oceania">Oceania</option>
+                </select>
+            </div>
+        </nav>
+    )
+}
