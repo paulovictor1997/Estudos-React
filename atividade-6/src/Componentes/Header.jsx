134f814310d6c9454b34854ace087d54253cf1d7
paulovictor1997 <paulovictornunes97@gmail.com>
2022-08-27 00:21:39 -0300

    Update atividade-6

--- atividade-6/src/Componentes/Header.jsx
+++ atividade-6/src/Componentes/Header.jsx
@@ -5,7 +5,9 @@ import './Header.css';
 export default function Header(){
     return(
         <header>
-            <h3 className="title">Where in the world ?</h3>
+            <h3 className="title">
+                Where in the world ?
+            </h3>
             <div className="toggle">
                 <BiMoon/>
                 <p>Change Mode</p>
