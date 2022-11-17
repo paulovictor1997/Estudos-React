cc526aa9fba18449d83ae189d598bc9c2575b5aa
paulovictor1997 <paulovictornunes97@gmail.com>
2022-11-16 21:46:04 -0300

    Update - Atividade-7

---
+++ atividade-7/data/question.js
@@ -0,0 +1,49 @@
+const data = [
+    {
+      question: "Quem foi o primeiro adversário de Seya na guerra galáctica?",
+      options: [
+        "Hyoga de Cisne",
+        "Geki de Urso",
+        "Shiryu de Dragão",
+        "Jabur de Unicórnio",
+      ],
+      answer: "Geki de Urso",
+    },
+    {
+      question: "Qual desses cavaleiros não é um cavaleiro de bronze?",
+      options: ["Misty ", "Jisty", "Ikki", "Shura"],
+      answer: "Misty",
+    },
+    {
+      question: "Qual dos cavaleiros de ouro dizia ser o mais leal a Athena?",
+      options: [
+        "Aiolos de Sargitário", 
+        "Shaka de Virgem", 
+        "Shura de Capricório", 
+        "Máscara da morte de Cancêr"
+        ],
+      answer: "Shura de Capricório",
+    },
+    {
+      question: "Em que local Ikki de Fênix foi treinado?",
+      options: [
+        "Cinco picos antigos", 
+        "Santuário", 
+        "Siberia", 
+        "Ilha da rainha da morte"
+    ],
+      answer: "Ilha da rainha da morte",
+    },
+    {
+      question:"Áries, Touro, Gêmeos... Seguindo a ordem, qual era a próxima casa do Zodiaco?",
+      options: [
+        "Leão", 
+        "Virgem", 
+        "Libra", 
+        "Câncer"
+    ],
+      answer: "Câncer",
+    },
+  ];
+  
+  export default data;
---
+++ atividade-7/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Os cavaleiros do Zodíaco - Quiz</h1>
+    </div>
+  )
+}
+
+export default App
