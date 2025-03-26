de52cea29903abf1a79717dc172578e1650970ea
Paulo Victor <81495904+paulovictor1997@users.noreply.github.com>
2025-03-26 12:27:12 -0300

    Update
    Adicionando um modal, aonde o usuário terá que confirmar se quer excluir o produto antes de excluir ele.

--- atividade-13/src/Pages/Cart.jsx
+++ atividade-13/src/Pages/Cart.jsx
@@ -1,16 +1,24 @@
-import {useContext }from 'react'
+import {useContext, useState }from 'react'
 import { CartContext } from '../context/CartContext'
-import { FaTrash } from 'react-icons/fa'
+import { FaTrash,FaCheck } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 
 import "./Cart.css"
 
 const Cart = () => {
-  const {cart, removeFromCart} = useContext(CartContext)  
+  const {cart, removeFromCart} = useContext(CartContext)
+  const [confirmDelete, setConfirmDelete] = useState(null)  
 
-  const handleRemove = (productId) => {
-    removeFromCart(productId);
-    toast.success('Produto removido do carrinho!')
+  const handleDelete = (productId) => {
+    setConfirmDelete(productId)
+  }
+
+  const confirmRemove = () => {
+    if(confirmDelete !== null){
+       removeFromCart(confirmDelete)
+       setConfirmDelete(null) 
+       toast.success('Produto removido do carrinho!')
+    }
   }
     
   return (
@@ -26,12 +34,22 @@ const Cart = () => {
                         <h3>{product.title}</h3>
                         <p><strong>Preço:</strong>${product.price.toFixed(2)}</p>
                     </div>
-                    <button onClick={()=> handleRemove(product.id)}  className='remove-btn'>
+                    <button onClick={()=> handleDelete(product.id)}  className='remove-btn'>
                         <FaTrash/>
                     </button>
                 </div>
             ))
         )}
+        {confirmDelete !== null && (
+        <>
+        <div className='modal-overlay' onClick={() => setConfirmDelete(null)}></div>
+        <div className='modal'>
+            <p>Deseja confirmar exclusão?</p>
+            <button onClick={confirmRemove} className='confirm-btn'><FaTrash/>Confirmar</button>
+            <button onClick={() => setConfirmDelete(null)} className='cancel-btn'> <FaCheck/>Cancelar</button>
+        </div>
+        </>
+    )}    
     </div>
   )
 }
