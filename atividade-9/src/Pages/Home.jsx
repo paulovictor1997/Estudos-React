9e53cd74fe1533c6d87d82cb060261271e169c38
Paulo Victor <81495904+paulovictor1997@users.noreply.github.com>
2025-07-25 12:22:57 -0300

    atividade-09 - Update

--- atividade-9/src/Pages/Home.jsx
+++ atividade-9/src/Pages/Home.jsx
@@ -1,53 +1,59 @@
-import {useState,useMemo,useEffect} from 'react'
-import "./Home.css"
-import CountryArea from '../componentes/CountryArea'
-import FilterByRegion from '../componentes/FilterByRegion'
-import Loader from '../componentes/Loader'
-import ScrollToTopButton from '../componentes/ScrollTopButton'
+import { useState, useMemo, useEffect } from 'react';
+import { BiError } from 'react-icons/bi';
+import "./Home.css";
+import CountryArea from '../componentes/CountryArea';
+import FilterByRegion from '../componentes/FilterByRegion';
+import Loader from '../componentes/Loader';
 
-export default function Home () {
-    
-    const [countries,setCountries] = useState([])
-    const [searchfield, setSearchField] = useState('')
+export default function Home() {
+  const [countries, setCountries] = useState([])
+  const [searchfield, setSearchField] = useState('')
+  const [isLoading, setLoading] = useState(false)
 
-    const countriesToRender = useMemo(()=>{
-        return countries.filter(c => c.name.common.toLowerCase().includes(searchfield.toLowerCase()))
-    },[searchfield, countries])
+  const countriesToRender = useMemo(() => {
+    return countries.filter(c =>
+      c.name.common.toLowerCase().includes(searchfield.toLowerCase())
+    );
+  }, [searchfield, countries])
 
-    const [isLoading, setLoading] = useState(false)
+  const loadUser = async () => {
+    setLoading(true);
+    const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags,region,population,capital');
+    const data = await response.json();
+    setCountries(data)
+    setLoading(false)
+  };
 
-    const loadUser = async()=>{
-      setLoading(true)
-      const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags,region,population,capital');
-      const data = await response.json();
-      //console.log(data)
-      setCountries(data)
-      setLoading(false)
-    }
+  useEffect(() => {
+    loadUser()
+  }, [])
 
-    useEffect(() => {
-        loadUser()
-    }, [])
-  
- 
-    return(
+  return (
     <>
-        <nav>
-            <form className='search-country' autoComplete='off'>
-                <input type="search" 
-                name='search' 
-                id='search' 
-                placeholder='Search for a country'
-                onChange={(e) => setSearchField(e.target.value)}
-                />
-            </form>
+      <nav>
+        <form className='search-country' autoComplete='off'>
+          <input
+            type="search"
+            name='search'
+            id='search'
+            placeholder='Search for a country'
+            onChange={(e) => setSearchField(e.target.value)}
+          />
+        </form>
 
-            <FilterByRegion setCountries={setCountries}/>
-            
-        </nav>
-        {isLoading && <Loader/>}
-        <CountryArea countries={countriesToRender} setCountries={setCountries} loadUser={loadUser}/>
-        <ScrollToTopButton/>
-    </>  
-    )
+        <FilterByRegion setCountries={setCountries} />
+      </nav>
+
+      {isLoading && <Loader />}
+
+      {!isLoading && countriesToRender.length === 0 ? (
+        <div className="not-found-message">
+          <BiError className="error-icon" />
+          <p>There is no country with this name.</p>
+        </div>
+      ) : (
+        <CountryArea countries={countriesToRender} setCountries={setCountries} loadUser={loadUser} />
+      )}
+    </>
+  );
 }
