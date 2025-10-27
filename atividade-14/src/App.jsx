8ed3e72667e6d35349ac4e91b70cd5e9fb1f460a
Paulo Victor <81495904+paulovictor1997@users.noreply.github.com>
2025-10-27 17:46:23 -0300

    Update
    Adicionando botão do dark mode

--- atividade-14/src/App.jsx
+++ atividade-14/src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from "react"
+import { useEffect,useState } from "react"
+import { FaMoon, FaSun } from "react-icons/fa"
 import SearchBox from "./componentes/SearchBox"
 import WeatherCard from "./componentes/WeatherCard"
 import "./index.css"
 
 function App() {
   const [weather,setWeather] = useState(null) 
+  const [darkMode,setDarkMode] = useState(()=>{
+    //Recupera do localstorage, por padrão false
+    try{
+      const storage = localStorage.getItem("weather_dark_mode")
+      return storage ? JSON.parse(storage) : false
+    } catch {
+      return false
+    }
+  })
+
+  useEffect(()=>{
+    document.body.classList.toggle("dark",darkMode)
+    localStorage.setItem("weather_dark_mode", JSON.stringify(darkMode))
+  },[darkMode])
+
+   const toggleDarkMode = () => setDarkMode((s) => !s)
  
   return (
    <div className="app-container">
       <h1 className="app-title">🌤️ Weather App</h1>
+      <button
+          aria-label="Alternar modo escuro"
+          className="mode-toggle"
+          onClick={toggleDarkMode}
+      >
+         {darkMode ? <FaSun /> : <FaMoon />}
+      </button>
       <SearchBox setWeather={setWeather} />
       {weather && <WeatherCard weather={weather} />}
     </div>
